d28d464c7d3f4950919429d1bc9a5e47e697ce2e
Seth Starr <starrseth@hotmail.com>
2020-06-08 21:41:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   new_array[]
   array.length.times do |munch|
-    new_array.push(array[munch])
+    new_array.push(array[munch]**2)
+  end 
+  new_array
   # your code here
 end
